181165814f72a4541913fdaf6e0894b800cd5b79
Bruno <bruno.moreira@meta.com.br>
2022-10-21 13:21:00 -0300

    feat: developed endpoint to create anf get user

---
+++ src/main/java/com/api/votingsession/Domain/Dtos/UserCreateDto.java
@@ -0,0 +1,13 @@
+package com.api.votingsession.Domain.Dtos;
+
+public class UserCreateDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
