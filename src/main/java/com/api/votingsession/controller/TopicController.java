5f367ab11fe86bbe86d5e54fbcdcdba9fc5e7196
Bruno <bruno.moreira@meta.com.br>
2023-01-15 10:16:33 -0300

    feat: topic endpoints

---
+++ src/main/java/com/api/votingsession/controller/TopicController.java
@@ -0,0 +1,38 @@
+package com.api.votingsession.controller;
+
+import com.api.votingsession.Repository.TopicRepository;
+import com.api.votingsession.application.service.TopicService;
+import com.api.votingsession.domain.Entity.Topic;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/topic")
+public class TopicController {
+
+    final TopicService topicService;
+
+    final TopicRepository topicRepository;
+
+    public TopicController(TopicService topicService, TopicRepository topicRepository) {
+        this.topicService = topicService;
+        this.topicRepository = topicRepository;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Request all topics", notes = "Search for all topics")
+    public ResponseEntity<List<Topic>> getAllTopics() {
+        return ResponseEntity.status(HttpStatus.OK).body(topicRepository.findAll());
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create new topic", notes = "To create a new topic, it must be valid within the system", hidden = true)
+    public ResponseEntity<Object> createNewTopic(String topic) {
+        return topicService.createNewTopic(topic);
+    }
+}
